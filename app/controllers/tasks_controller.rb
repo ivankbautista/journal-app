2ae19853f702ab2424a83857d07e20ae8c94f962
ivankbautista <ivankevinbautista@gmail.com>
2021-10-12 15:37:42 +0800

    Fix bug on destroy method not working properly

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -29,9 +29,10 @@ class TasksController < ApplicationController
     end
 
     def destroy
+        @category = Category.find(params[:category_id])
         @task = @category.tasks.find(params[:id])
         @task.destroy
-        redirect_to categories_path(@category)
+        redirect_to category_path(@category)
     end
 
     private
