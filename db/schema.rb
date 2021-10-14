a9ac1deae87f38bb77248e14b641831b233ff127
ivankbautista <ivankevinbautista@gmail.com>
2021-10-14 21:32:39 +0800

    Add in user_id for categories

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_13_132127) do
+ActiveRecord::Schema.define(version: 2021_10_14_122942) do
 
   create_table "categories", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_categories_on_user_id"
   end
 
   create_table "tasks", force: :cascade do |t|
