1d811777ec50c704aa7a908f5d29f68a9e290534
ivankbautista <ivankevinbautista@gmail.com>
2021-10-18 21:54:35 +0800

    Add in test when creating tasks, and saving without titles

--- test/models/task_test.rb
+++ test/models/task_test.rb
@@ -1,7 +1,20 @@
 require "test_helper"
 
 class TaskTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user = users(:ivan)
+  end
+
+  test 'should not save Task without title' do
+    category = @user.categories.create(title: "School")
+    task = category.tasks.create(title: nil)
+
+    assert_not task.save, "saved task without title"
+  end
+
+  test "" do
+    # setup
+    # exercise
+    # verify
+  end
 end
